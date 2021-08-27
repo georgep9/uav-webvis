5b5b0bf65e538fccd6213d26ecf4a0d4343e3a0c
georgep9 <g-patt@outlook.com>
2021-08-27 12:21:04 +1000

    init server

--- server/app.py
+++ server/app.py
@@ -1,5 +1,11 @@
-import flask from Flask
+from flask import Flask
 
+app = Flask(__name__)
 
+@app.route('/')
+def hw():
+    return "<h1>Hello, world</h1>"
 
-
+@app.route('/api')
+def api():
+    return "<h1>Hello, api</h1>"
