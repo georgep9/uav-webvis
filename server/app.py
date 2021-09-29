661d205f0565b3732eda0c308dd145ad3ca3cc83
George Patterson <g-patt@outlook.com>
2021-09-30 01:25:37 +1000

    updated /api/aq/live route to query dynamodb

--- server/app.py
+++ server/app.py
@@ -1,8 +1,10 @@
 from flask import Flask, jsonify, request
 from flask_cors import CORS
 from waitress import serve
+import boto3
+from boto3.dynamodb.conditions import Key
+import simplejson as json
 import logging
-
 import random
 import time
 
@@ -18,26 +20,29 @@ def hw():
 
 @app.route('/api/aq/live', methods=['GET'])
 def serve_aq_live():
+    dynamodb = boto3.resource('dynamodb')
+    table = dynamodb.Table("uav_wvi")
 
-    ts = round(time.time() * 1000)
+    try:
+        response = table.query(
+            KeyConditionExpression=Key('data_type').eq("air_quality"),
+            ScanIndexForward=True,
+            Limit=1
+        )
+        item = response["Items"][0]
 
-    aq_sens = {
-        "temp": {},
-        "press": {},
-        "hum": {},
-        "light": {},
-        "noise": {},
-        "ox": {},
-        "red": {},
-        "nh3": {}
-    }
-    for sen in aq_sens:
-        val = round(random.uniform(0, 1), 2)
-        aq_sens[sen] = {'val': val}
+        ts = item["timestamp"]
+        sensors = {}
+        for sensor in item.keys():
+            if sensor != "timestamp" and sensor != "data_type":
+                sensors[sensor] = {'val': item[sensor]}
+                
+        aq_live = {'ts': ts, 'sensors': sensors}
+        return json.dumps(aq_live)
 
-    live_aq = {'ts': ts, 'sensors': aq_sens}
-
-    return jsonify(live_aq)
+    except Exception as e:
+        print(e)
+        return "something went wrong"
 
 @app.route('/api/aq/sen', methods=['GET'])
 def serve_aq_sen():
@@ -52,7 +57,7 @@ def serve_aq_sen():
 
     sen_data = {'ts': ts, 'val': norm_val}
 
-    return jsonify(sen_data)
+    return json.dumps(sen_data)
 
 if __name__ == "__main__":
     #app.run(host='0.0.0.0')
