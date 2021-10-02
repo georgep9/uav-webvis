259957d736a8694aedad1413e5f5bbf423c2634a
georgep9 <g-patt@outlook.com>
2021-10-02 15:53:56 +1000

    changes to aq data structure and update routine

--- client/src/components/AirQuality.vue
+++ client/src/components/AirQuality.vue
@@ -1,11 +1,11 @@
 <template>
   <div id="airquality">
 
-    <div id="aq-live" v-if="barData !== null">
-      <AQBarChart id="aq-bar-chart" :aqData="barData" />
+    <div id="aq-live" v-if="charts.live.barData !== null">
+      <AQBarChart id="aq-bar-chart" :aqData="charts.live.barData" />
     </div>
 
-    <div id="aq-history" v-if="lineData !== null">
+    <div id="aq-history" v-if="charts.hist.lineData !== null">
 
       <div id="sensor-buttons">
         <button id="sensor-button" type="button"
@@ -16,8 +16,8 @@
             width:12%;
             padding:0
           "
-          v-for="sensor in sensors" :key="sensor" :ref="sensor"
-          v-bind:class="[selected === sensor ? 'btn btn-primary' : 'btn btn-secondary']"
+          v-for="sensor in charts.sensors" :key="sensor" :ref="sensor"
+          v-bind:class="[charts.selected === sensor ? 'btn btn-primary' : 'btn btn-secondary']"
           v-on:click="changeHistory(sensor)">
           {{ sensor.toUpperCase() }}
         </button>
@@ -25,8 +25,8 @@
 
       <AQLineChart 
         id="aq-line-chart" 
-        :sensorName="selected" 
-        :histData="lineData" 
+        :sensorName="charts.selected" 
+        :histData="charts.hist.lineData" 
         style="padding-top: 20px"
       />
     
@@ -42,7 +42,7 @@ import AQBarChart from './AQBarChart.vue';
 import AQLineChart from './AQLineChart.vue';
 import time from '../assets/js/time-func.js'
 
-const updateDelay = 50;
+//const updateDelay = 1;
 
 export default {
 
@@ -54,107 +54,73 @@ export default {
   },
 
   data: () => ({
-    sensors: [],
-    selected: null,
+    
+    api: {
+      lastTs: 0,
+      liveData: {new: false, data: null},
+      histData: {new: false, data: null}
+    },
 
-    lastTs: 0,
-    liveData: {new: false, data: null},
-    histData: {new: false, data: null},
-    refreshHist: true,
-
-    barData: null,
-    barDataMax: {},
-    barDataMin: {},
-
-    lineData: new Array(
-      new Array(), 
-      new Array()
-    ),
-
-    maxHistSamples: 20
+    charts: {
+      sensors: [],
+      selected: null,
+      refreshHist: false,
+      live: {
+        barData: null,
+        barDataMax: {},
+        barDataMin: {}
+      },
+      hist: {
+        maxHistSamples: 20,
+        lineData: new Array(
+          new Array(), 
+          new Array()
+        )
+      },
+    }
+    
   }),
 
-
   mounted() {
-    setTimeout(this.updateAirQuality, Math.round(updateDelay));
+    //setTimeout(this.update, updateDelay);
+    this.update();
   },
-
 
   methods: {
 
-    updateAirQuality: async function () {
-
-      await this.fetchData();
-      this.updateChartData();
-      setTimeout(this.updateAirQuality, updateDelay);
-
-    },
-
-    changeHistory: function(sensor) {
-      if (sensor === this.selected) { return; }
-
-      this.lineData = new Array(
-        new Array(), 
-        new Array()
-      );
-      this.selected = sensor;
-      this.refreshHist = true
+    update: async function () {
+      for(;;) {
+        await this.fetchData();
+        this.updateChartData();
+      }
     },
 
     fetchData: async function () {
-
-      let fetchHist = false;
-      if (this.selected !== null && this.refreshHist) {
-        fetchHist = true;
-        this.refreshHist = false;
+      let dataToFetch = "live";
+      if (this.charts.refreshHist) {
+        dataToFetch = "hist"; 
+        this.charts.refreshHist = false;
       }
 
-      if (fetchHist === true){
-        const fetchedData = await this.fetchSenHistData();
-        if (fetchedData) { this.histData = { new:true, data:fetchedData }; }
+      if (dataToFetch === "live") {
+        const newLiveData = await this.fetchLiveData();
+        if (newLiveData) { 
+          this.api.liveData = {new:true, data:newLiveData}; 
+          this.api.lastTs = newLiveData[newLiveData.length - 1].ts;
+        }
       }
-      else {
-        const fetchedData = await this.fetchLiveData();
-        if (fetchedData) { this.liveData = { new:true, data:fetchedData }; }
+      else if (dataToFetch === "hist") {
+        const newHistData = await this.fetchSenHistData();
+        if (newHistData) { 
+          this.api.histData = {new:true, data:newHistData}; 
+          this.api.lastTs = newHistData[newHistData.length - 1].ts;
+        }
       }
-
     },
-
-
-    updateChartData: function() {
-
-      if (this.liveData.new === false && this.histData.new === false)
-      { return; }
-
-      const lData = JSON.parse(JSON.stringify(this.liveData.data));
-      const hData = JSON.parse(JSON.stringify(this.histData.data));
-      const lNew = this.liveData.new;
-      const hNew = this.histData.new;
-      this.sensors = Object.keys(lData[0].sensors);
-
-      this.liveData.new = false;
-      this.histData.new = false;
-
-      if (lNew === true) { 
-        this.lastTs = lData[lData.length - 1].ts
-        this.updateBarData(lData);
-        if (this.selected !== null)
-        { this.updateLineData(lData); }
-      }
-      else if (hNew === true){
-        this.lastTs = hData[hData.length - 1].ts
-        this.updateLineData(hData);
-      }
-
-      if (this.selected === null) 
-      { this.selected = this.sensors[0]; }
-  
-    },
-
 
     fetchLiveData: async function() {
       const apiEndpoint = `${process.env.VUE_APP_API_HOST}/api/aq/live`;
-      const apiArgs = `?from_ts=${this.lastTs}`;
+      const apiArgs = `?from_ts=${this.api.lastTs}`;
       const apiUrl = apiEndpoint + apiArgs ;
 
       let apiData;
@@ -165,10 +131,10 @@ export default {
       return apiData;
     },
 
-
     fetchSenHistData: async function() {
       const apiEndpoint= `${process.env.VUE_APP_API_HOST}/api/aq/sen`
-      const apiArgs = `?sensor=${this.selected}&samples=${this.maxHistSamples}`;
+      let apiArgs = `?sensor=${this.charts.selected}`
+      apiArgs += `&samples=${this.charts.hist.maxHistSamples}`;
       const apiUrl = apiEndpoint + apiArgs;
 
       let apiData;
@@ -178,45 +144,83 @@ export default {
       return apiData;
     },
 
-    
+    updateChartData: function() {
+      const liveData = this.api.liveData;
+      const histData = this.api.histData;
+      if (liveData.new === false && histData === false) { return; }
+
+      this.charts.sensors = Object.keys(liveData.data[0].sensors);
+
+      if (liveData.new) { 
+        this.updateBarData(liveData.data);
+        if (this.charts.selected !== null) {
+          this.updateLineData(liveData.data);
+        }
+      }
+      else if (histData.new) {
+        this.updateLineData(histData.data);
+      }
+
+      if (this.charts.selected === null) { 
+        this.charts.selected = this.charts.sensors[0]; 
+      }
+
+      this.api.liveData.new = false;
+      this.api.histData.new = false;
+    },
+
     updateBarData: function (data) {
       const sensorsData = data[data.length - 1].sensors;
 
       var newBarData = new Map();
-      this.sensors.forEach(sensor => {
+      this.charts.sensors.forEach(sensor => {
         const val = sensorsData[sensor].val;
         
         // update normalisation scales
-        if (!Object.prototype.hasOwnProperty.call(this.barDataMax, sensor) ||
-            this.barDataMax[sensor] < val)
-        { this.barDataMax[sensor] = val; }
+        if (!Object.prototype.hasOwnProperty
+              .call(this.charts.live.barDataMax, sensor) ||
+            this.charts.live.barDataMax[sensor] < val) { 
+          this.charts.live.barDataMax[sensor] = val;
+        }
+        if (!Object.prototype.hasOwnProperty
+              .call(this.charts.live.barDataMin, sensor) ||
+            this.charts.live.barDataMin[sensor] > val) {
+          this.charts.live.barDataMin[sensor] = val;
+        }
 
-        if (!Object.prototype.hasOwnProperty.call(this.barDataMin, sensor) ||
-            this.barDataMin[sensor] > val)
-        { this.barDataMin[sensor] = val; }
-
-        const scaledVal = val / this.barDataMax[sensor] * 100;
+        const scaledVal = val / this.charts.live.barDataMax[sensor] * 100;
         newBarData.set(sensor, {'normal': val, 'scaled': scaledVal})
       });
 
-      this.barData = newBarData;
+      this.charts.live.barData = newBarData;
     },
-
 
     updateLineData: function(data) {
       data.forEach(sample => {
         const unixTs = sample.ts;
         const ts = time.getTimestamp(new Date(unixTs))
-        const val = sample.sensors[this.selected].val;
+        const val = sample.sensors[this.charts.selected].val;
 
-        if (this.lineData[0].length >= this.maxHistSamples) { 
-          this.lineData[0].shift();
-          this.lineData[1].shift();
+        if (this.charts.hist.lineData[0].length >= 
+            this.charts.hist.maxHistSamples) { 
+          this.charts.hist.lineData[0].shift();
+          this.charts.hist.lineData[1].shift();
         }
-        this.lineData[0].push(ts);
-        this.lineData[1].push(val);
+        this.charts.hist.lineData[0].push(ts);
+        this.charts.hist.lineData[1].push(val);
       })
     },
+
+    changeHistory: function(sensor) {
+      if (sensor === this.charts.selected) { return; }
+
+      this.charts.hist.lineData = new Array(
+        new Array(), 
+        new Array()
+      );
+      this.charts.selected = sensor;
+      this.charts.refreshHist = true
+    }
 
   }
 }
