6bd042176e4e8cf61295fcaa1a954a3b3fcfb260
George Patterson <g-patt@outlook.com>
2021-10-12 19:57:03 +1000

    Refactored AQ server routes.

--- server/app.py
+++ server/app.py
@@ -8,14 +8,13 @@ import logging
 import time
 import redis
 
+import aq
+
 app = Flask(__name__)
 CORS(app)
 
 logger = logging.getLogger('waitress')
 logger.setLevel(logging.DEBUG)
-
-db_ttl_min = 5 # time to live in minutes
-query_lim_max = 100 # max db items to query
 
 cache = redis.Redis(host="localhost", port=6379)
 cache_ttl = 100 # time to live in milliseconds
@@ -31,161 +30,26 @@ ip_hist_route = '/api/ip/hist'
 def hw():
     return "<h1>Hello, world</h1>"
 
-@app.before_request
-def check_cache():
-    method = request.method
-    path = request.path
-    ts = request.args.get('from_ts')
-
-    if (method == "GET" and 
-        path == aq_live_route and
-        ts is not None):
-        data = cache.get(aq_live_route+'/'+ts)
-        if data is not None:
-            print("[GET " + aq_live_route + "] Cache hit! Returning data.")
-            return data
-
-@app.after_request
-def update_cache(res):
-    method = request.method
-    path = request.path
-    ts = request.args.get('from_ts')
-    data = res.get_data()
-    
-    if (method == "GET" and 
-        path == aq_live_route and
-        ts is not None and
-        type(json.loads(data)) is list):
-
-        new_key = aq_live_route+'/'+ts
-        cache.set(new_key, data, px=cache_ttl, nx=True)
-
-    return res
-
-# convert db items list to 
-# json response for front-end
-def process_json_res(db_items):
-    samples = []
-    for item in db_items:
-        ts = item["timestamp"]
-        sensors = {}
-        for sensor in item.keys():
-            if sensor != "timestamp" \
-            and sensor != "data_type" \
-            and sensor != "ttl":
-                sensors[sensor] = {'val': item[sensor]} 
-        sample = {'ts': ts, 'sensors': sensors}
-        samples.append(sample)
-
-    response = json.dumps(samples)
-    length = len(samples)
-    return (response, length)
-
 
 @app.route(aq_live_route, methods=['GET'])
 def get_aq_live():
-    dynamodb = boto3.resource('dynamodb')
-    table = dynamodb.Table("uav_wvi")
-
-    try:
-        query_limit = 1
-        from_ts = 0
-
-        from_ts_arg = request.args.get('from_ts')
-        if from_ts_arg is not None:
-            from_ts = int(from_ts_arg)
-            if from_ts != 0: query_limit = query_lim_max
-        
-        start_t = round(time.time() * 1000)
-        db_res = table.query(
-            KeyConditionExpression=
-                Key('data_type').eq("air_quality") &
-                Key('timestamp').gt(from_ts),
-            ScanIndexForward=False,
-            Limit=query_limit)
-        db_items = reversed(db_res["Items"])
-        
-        json_res, length = process_json_res(db_items)
-        end_t = round(time.time() * 1000)
-        query_dur = end_t - start_t
-        print('[GET ' + aq_live_route + '] DDB Query time: ' + str(query_dur) + ' ms. ' + \
-            'Serving ' + str(length) + ' samples.')
-        return json_res
-
-    except ValueError as e:
-        print(e)
-        return json.dumps("[ERR] Argument 'from_ts' must be an integer of unix time.")
-
-    except Exception as e:
-        print(e)
-        return json.dumps("[ERR] Execption caught while querying database.")
+    ts = request.args.get('from_ts')
+    cache_res = aq.check_cache(cache, ts, aq_live_route)
+    if cache_res: return cache_res
+    else: return aq.get_live(ts, aq_live_route)
 
 
 @app.route(aq_sen_route, methods=['GET'])
 def get_aq_sen():
-    dynamodb = boto3.resource('dynamodb')
-    table = dynamodb.Table("uav_wvi")
-
     sensor = request.args.get('sensor')
     samples = request.args.get('samples')
-
-    if samples is None or sensor is None:
-        return json.dumps("[ERR] Must provide 'sensor' and 'samples' argument.")
-
-    try:
-        samples = int(samples)
-        if samples < 1 or samples > 500: raise Exception
-    except:
-        return json.dumps("[ERR] Argument 'samples' must be an integer between 1 and 500.")
-
-    try:
-        start_t = round(time.time() * 1000)
-        db_res = table.query(
-            ProjectionExpression="#ts, #sen",
-            ExpressionAttributeNames={"#ts": "timestamp", "#sen": sensor},
-            KeyConditionExpression=Key('data_type').eq("air_quality"),
-            ScanIndexForward=False,
-            Limit=samples)
-        db_items = reversed(db_res["Items"])
-        json_res, length = process_json_res(db_items)
-
-        end_t = round(time.time() * 1000)
-        query_dur = end_t - start_t
-        print('[GET ' + aq_live_route + '] DDB Query time: '  + str(query_dur) + ' ms. ' + \
-            'Serving ' + str(length) + " samples for " + sensor + ".")
-        return json_res
-
-    except Exception as e:
-        print(e)
-        return json.dumps("[ERR] Execption caught while querying database.")
+    return aq.get_sen(aq_sen_route, sensor, samples)
 
 
 @app.route(aq_post_route, methods=["POST"])
 def post_aq():
-
-    post_json = json.loads(request.get_json())
-
-    if "ts" not in post_json and "data" not in post_json:
-        return json.dumps("[ERR] Bad format.")
-
-    ts = post_json["ts"]
-    sensors = post_json["data"]
-    ttl = {'ttl': int(time.time()) + 60 * db_ttl_min}
-
-    new_db_item = {"data_type": "air_quality", "timestamp": ts}
-    new_db_item.update(sensors)
-    new_db_item.update(ttl)
-    new_db_item = json.loads(json.dumps(new_db_item), use_decimal=True)
-
-    try:
-        dynamodb = boto3.resource('dynamodb')
-        table = dynamodb.Table("uav_wvi")
-        table.put_item(Item=new_db_item)
-        return json.dumps("Success")
-
-    except Exception as e:
-        print(e)
-        return json.dumps("[ERR] Execption caught while creating item in database.")
+    data = json.loads(request.get_json())
+    return aq.post(data)
 
 
 @app.route(ip_live_route , methods=["GET", "POST"])
---
+++ server/aq.py
@@ -0,0 +1,142 @@
+import boto3
+from boto3.dynamodb.conditions import Key
+import simplejson as json
+import time
+
+
+cache_ttl = 100 # milliseconds
+db_ttl_min = 5 # minutes
+query_lim_max = 100 # max db items to query
+
+
+def check_cache(cache, ts, path):
+    if (ts is not None):
+        data = cache.get(path+'/'+ts)
+        if data is not None:
+            print("[GET " + path + "] Cache hit! Returning data.")
+            return data
+
+
+def update_cache(cache, ts, path, data):
+    if (ts is not None and
+        type(json.loads(data)) is list):
+      new_key = path+'/'+ts
+      cache.set(new_key, data, px=cache_ttl, nx=True)
+
+
+def process_json_res(db_items):
+    samples = []
+    for item in db_items:
+        ts = item["timestamp"]
+        sensors = {}
+        for sensor in item.keys():
+            if sensor != "timestamp" \
+            and sensor != "data_type" \
+            and sensor != "ttl":
+                sensors[sensor] = {'val': item[sensor]} 
+        sample = {'ts': ts, 'sensors': sensors}
+        samples.append(sample)
+
+    response = json.dumps(samples)
+    length = len(samples)
+    return (response, length)
+
+
+def get_live(ts, path):
+    try:
+        query_limit = 1
+        from_ts = int(ts)
+        if from_ts != 0: query_limit = query_lim_max
+        if from_ts < 0: raise Exception
+    except Exception as e:
+      return json.dumps("[ERR] Argument 'from_ts' must be an integer of unix time.")
+
+    try:
+        dynamodb = boto3.resource('dynamodb')
+        table = dynamodb.Table("uav_wvi")
+        
+        start_t = round(time.time() * 1000)
+
+        db_res = table.query(
+            KeyConditionExpression=
+                Key('data_type').eq("air_quality") &
+                Key('timestamp').gt(from_ts),
+            ScanIndexForward=False,
+            Limit=query_limit)
+
+        end_t = round(time.time() * 1000)
+        query_dur = end_t - start_t
+
+        db_items = reversed(db_res["Items"])
+        json_res, length = process_json_res(db_items)
+
+        print('[GET ' + path + '] DDB Query time: ' + str(query_dur) + ' ms. ' + \
+            'Serving ' + str(length) + ' samples.')
+        return json_res
+
+    except Exception as e:
+        print(e)
+        return json.dumps("[ERR] Execption caught while querying database.")
+
+
+def get_sen(path, sensor, samples):
+    if samples is None or sensor is None:
+        return json.dumps("[ERR] Must provide 'sensor' and 'samples' argument.")
+
+    try:
+        samples = int(samples)
+        if samples < 1 or samples > 500: raise Exception
+    except:
+        return json.dumps("[ERR] Argument 'samples' must be an integer between 1 and 500.")
+
+    try:
+        dynamodb = boto3.resource('dynamodb')
+        table = dynamodb.Table("uav_wvi")
+
+        start_t = round(time.time() * 1000)
+
+        db_res = table.query(
+            ProjectionExpression="#ts, #sen",
+            ExpressionAttributeNames={"#ts": "timestamp", "#sen": sensor},
+            KeyConditionExpression=Key('data_type').eq("air_quality"),
+            ScanIndexForward=False,
+            Limit=samples)
+
+        end_t = round(time.time() * 1000)
+        query_dur = end_t - start_t
+
+        db_items = reversed(db_res["Items"])
+        json_res, length = process_json_res(db_items)
+        
+        print('[GET ' + path + '] DDB Query time: '  + str(query_dur) + ' ms. ' + \
+            'Serving ' + str(length) + " samples for " + sensor + ".")
+        return json_res
+
+    except Exception as e:
+        print(e)
+        return json.dumps("[ERR] Execption caught while querying database.")
+
+
+def post(data):
+    try:
+        ts = data["ts"]
+        sensors = data["data"]
+    except Exception as e:
+        print(e)
+        return json.dumps("[ERR] Bad format.")
+    
+    ttl = {'ttl': int(time.time()) + 60 * db_ttl_min}
+
+    new_db_item = {"data_type": "air_quality", "timestamp": ts}
+    new_db_item.update(sensors)
+    new_db_item.update(ttl)
+    new_db_item = json.loads(json.dumps(new_db_item), use_decimal=True)
+
+    try:
+        dynamodb = boto3.resource('dynamodb')
+        table = dynamodb.Table("uav_wvi")
+        table.put_item(Item=new_db_item)
+        return json.dumps("Success")
+    except Exception as e:
+        print(e)
+        return json.dumps("[ERR] Execption caught while creating item in database.")
